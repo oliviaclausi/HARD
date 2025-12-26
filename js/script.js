6151fe05c37c3e64cfc26ecd8e7f6fd687397c97
oliviaclausi <oliviaclausi@gmail.com>
2025-12-26 13:59:28 -0500

    members page

---
+++ js/script.js
@@ -0,0 +1,17 @@
+﻿// Use the module syntax to export the function
+export function sayHi(name) {
+    alert(`hello ${name}!`);
+}
+
+export function downloadCSV(filename, content) {
+	var element = document.createElement('a');
+	element.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(content));
+	element.setAttribute('download', filename);
+
+	element.style.display = 'none';
+	document.body.appendChild(element);
+
+	element.click();
+
+	document.body.removeChild(element);
+}
